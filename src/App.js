5a2d85f97eefbb83f87f082076aa3cfdaba80dba
Kyle Deato <kyledeato@gmail.com>
2022-05-04 14:12:43 -0700

    add onClick event

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Names from './components/Names';
 function App() {
   return (
     <div className="App">
-      <Names lastName="Doe" firstName="John" age ="45" color="black"/>
-      <Names lastName="Smith" firstName="Jane" age ="33" color="brown"/>
-      <Names lastName="Wayne" firstName="Bruce" age ="22" color="black"/>
-      <Names lastName="Smooth" firstName="Bill" age ="55" color="brown"/>
+      <Names lastName="Doe" firstName="John" age ={45} color="black"/>
+      <Names lastName="Smith" firstName="Jane" age ={33} color="brown"/>
+      <Names lastName="Wayne" firstName="Bruce" age ={22} color="black"/>
+      <Names lastName="Smooth" firstName="Bill" age ={11} color="brown"/>
     </div>
   );
 }
--- src/components/Names.js
+++ src/components/Names.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 
 class Names extends Component{
+    constructor(props) {
+        super(props);
+        this.state ={
+            age: this.props.age
+        }
+    }
     render(){
-        const {firstName, lastName, age, color} = this.props;
+        const {firstName, lastName, color} = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {color}</p>
+                <button onClick={ ()=> {this.setState({age: this.state.age+1})} }>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
